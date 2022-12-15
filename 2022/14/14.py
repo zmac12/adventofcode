6c0fd1e2f880b7eebcbaba780b06356187d3bec3
Zach McQuiston <zach.mcquiston35@gmail.com>
2022-12-15 11:36:35 -0700

    refactoring coordinate generation function for readability

--- 2022/14/14.py
+++ 2022/14/14.py
@@ -2,25 +2,31 @@ from typing import TypeAlias
 
 Coordinate: TypeAlias = tuple[int, int]
 
-def convert_to_coord_add_to_set(coord_list:list(str), container:set):
+def convert_to_coord_add_to_set(coord_list:list[str], container:set):
     for idx,coord in enumerate(coord_list):
-        coord_list[idx] = (int(coord.split(',')[0]), int(coord.split(',')[1]))
+        x,y = int(coord.split(',')[0]), int(coord.split(',')[1])
+        coord_list[idx] = (x, y)
     for idx in range(0, len(coord_list) - 2):
-        if coord_list[idx][0] != coord_list[idx + 1][0]:
-            start = min(coord_list[idx][0], coord_list[idx + 1][0])
-            end = max(coord_list[idx][0], coord_list[idx + 1][0])
-            constant = coord_list[idx][1]
+        x, y = coord_list[idx][0], coord_list[idx][1]
+        x_1, y_1 = coord_list[idx + 1][0], coord_list[idx + 1][1]
+        if x != x_1:
+            start = min(x, x_1)
+            end = max(x, x_1)
+            constant = y
             mode = 'horizontal'
         else:
-            start = min(coord_list[idx][1], coord_list[idx + 1][1])
-            end = max(coord_list[idx][1], coord_list[idx + 1][1])
-            constant = coord_list[idx][0]
+            start = min(y, y_1)
+            end = max(y, y_1)
+            constant = x
             mode = 'vertical'
         for num in range(start, end + 1):
             if mode == 'horizontal':
-                container.add(num, constant)
+                container.add((num, constant))
             elif mode == 'vertical':
-                container.add(constant, num)
+                container.add((constant, num))
+
+def simulate_sand():
+
 
 def main():
     with open('input.txt', 'r') as file:
