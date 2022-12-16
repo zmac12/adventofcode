a446c12a6e298c1f1c50326620c1bf0a0f114c60
Zach McQuiston <zach.mcquiston35@gmail.com>
2022-12-15 17:45:43 -0700

    Adding simulate sand code before testing

--- 2022/14/14.py
+++ 2022/14/14.py
@@ -2,9 +2,19 @@ from typing import TypeAlias
 
 Coordinate: TypeAlias = tuple[int, int]
 
+max_x, max_y, min_x, min_y = 0
+
 def convert_to_coord_add_to_set(coord_list:list[str], container:set):
     for idx,coord in enumerate(coord_list):
         x,y = int(coord.split(',')[0]), int(coord.split(',')[1])
+        if x > max_x:
+            max_x = x
+        elif x < min_x and x>= 0:
+            min_x = x
+        if y > max_y:
+            max_y = y
+        elif y < min_y and y>= 0:
+            min_y = y
         coord_list[idx] = (x, y)
     for idx in range(0, len(coord_list) - 2):
         x, y = coord_list[idx][0], coord_list[idx][1]
@@ -25,7 +35,19 @@ def convert_to_coord_add_to_set(coord_list:list[str], container:set):
             elif mode == 'vertical':
                 container.add((constant, num))
 
-def simulate_sand():
+def simulate_sand(container:set):
+    start_x, start_y = 500, 0
+
+    abyss = False
+    while abyss == False:
+        #drop sand
+        sand_location = (start_x, start_y)
+        while sand_location[0] <= max_x and sand_location[1] <= max_y and sand_location[0] >= min_x and sand_location[1] >= min_y:
+            if sand_location in container:
+                sand_location = (sand_location[0] - 1, sand_location[1] + 1)
+            else:
+                container.add((sand_location[0], sand_location[1]))
+        abyss == True
 
 
 def main():
@@ -38,7 +60,7 @@ def main():
     for list_of_corners in corners:
         convert_to_coord_add_to_set(list_of_corners, filled)
     
-    print(filled)
+    simulate_sand(filled)
     
 
 if __name__ == '__main__':
