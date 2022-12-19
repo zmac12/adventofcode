55cc3deafecb83133cedb8232aa05906de0c14e2
Zach McQuiston <zach.mcquiston35@gmail.com>
2022-12-19 14:00:47 -0700

    Submit part 2 of day 2 solution

--- 2022/2/2_2.py
+++ 2022/2/2_2.py
@@ -8,4 +8,20 @@ win = 6
 draw = 3
 loss = 0
 
+wins = {'A': 8, 'B': 9, 'C': 7}
+losses = {'A': 3, 'B': 1, 'C': 2}
+draws = {'A': 4, 'B': 5, 'C': 6}
+
+
 totalScore = 0
+
+for game in games:
+    plays = game.split(' ')
+    if plays[1] == 'X':
+        totalScore += losses.get(plays[0])
+    elif plays[1] == 'Y':
+        totalScore += draws.get(plays[0])
+    else:
+        totalScore += wins.get(plays[0])
+
+print(totalScore)
