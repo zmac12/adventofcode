77168e14fd731c76258ea92a48d23838d0fbdfac
Zach McQuiston <zach.mcquiston35@gmail.com>
2022-12-20 10:20:47 -0700

    Adding completed code for day 4 part 1

--- 2022/4/4.py
+++ 2022/4/4.py
@@ -1,8 +1,17 @@
 with open('input.txt', 'r') as file:
     lines = file.read().split('\n')
 
+fullyContainedPairs = 0
+
 for window in lines:
     window = window.split(',')
     c1, c2 = window[0], window[1]
     c1, c2 = [int(num) for num in c1.split('-')], [int(num) for num in c2.split('-')]
-    
+    if c1[0] >= c2[0] and c1[1] <= c2[1]:
+        fullyContainedPairs += 1
+    elif c2[0] >= c1[0] and c2[1] <= c1[1]:
+        fullyContainedPairs += 1
+    else:
+        fullyContainedPairs += 0
+
+print(fullyContainedPairs)
