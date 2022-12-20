5925d6b5d7d3b55c97befc1c03e5b827414a9947
Zach McQuiston <zach.mcquiston35@gmail.com>
2022-12-19 21:29:12 -0700

    day four part 1

--- 2022/3/3_2.py
+++ 2022/3/3_2.py
@@ -1,2 +1,17 @@
 with open('input.txt', 'r') as file:
     lines = file.read().split('\n')
+
+badges = []
+
+for i in range(0, len(lines) - 2, 3):
+    badge = 0
+    c1, c2, c3 = [*lines[i]], [*lines[i+1]], [*lines[i+2]]
+    for item in c1:
+        if item in c2 and item in c3:
+            if item.islower():
+                badge += ord(item) - ord('a') + 1
+            if item.isupper():
+                badge += ord(item) - 65 + 27
+            break
+    badges.append(badge)
+print(sum(badges))
---
+++ 2022/4/4.py
@@ -0,0 +1,3 @@
+with open('input.txt', 'r') as file:
+    lines = file.read().split('\n')
+
