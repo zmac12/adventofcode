f640ccf52b0d0d16504800c8bca13dd9214fb202
Zach <zach.mcquiston35@gmail.com>
2022-12-19 22:53:03 -0700

    Adding parsing to the input

--- 2022/4/4.py
+++ 2022/4/4.py
@@ -1,3 +1,8 @@
 with open('input.txt', 'r') as file:
     lines = file.read().split('\n')
 
+for window in lines:
+    window = window.split(',')
+    c1, c2 = window[0], window[1]
+    c1, c2 = [int(num) for num in c1.split('-')], [int(num) for num in c2.split('-')]
+    
